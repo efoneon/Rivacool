d9cbd32e308e1b623fafff14fa13e4991cd4331b
efoneon <115902510+efoneon@users.noreply.github.com>
2023-10-08 09:15:20 -0400

    Created a maximum num and minimum

--- num-generator/generator.js
+++ num-generator/generator.js
@@ -2,7 +2,9 @@ const number = document.getElementById('number');
 const button = document.getElementById('button-to-click');
 
 function generate() {
-  const generatedNumber = Math.random();
+  const min = 1;
+  const max = 100;
+  const generatedNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   number.textContent = generatedNumber;
 }
 
